174623aaa0b3ba1d0b41bae5cf7f365108ed1b2c
Elie Drai <elie.drai.study@gmail.com>
2019-03-18 12:30:41 +0200

    translate

--- Compiler/Exercise1/ex1.swift
+++ Compiler/Exercise1/ex1.swift
@@ -16,6 +16,68 @@ func ex1_introduction(){
     print("---------------------")
 }
 
+func push(command_list: [Substring]) -> String{
+    /*
+     Function to translate push command
+     */
+    var operation = ""
+    
+    switch(command_list[1]){
+        
+    case "constant":
+        //  the constant case
+        operation = "@\(command_list[2])\n"
+        operation+="D=A\n"
+        operation+="@SP\n"
+        operation+="A=M\n"
+        operation+="M=D\n"
+        operation+="@SP\n"
+        operation+="M=M+1\n"
+        
+    default:
+        //  default case
+        return ""
+    }
+    
+    return operation
+}
+
+
+func translate(command: String)->String{
+    /*
+     Function to translate every kind of command from vm to hack
+     */
+    let command_list = command.split(separator: " ")
+    
+    if command_list[0] == "//"{
+        return ""
+    }
+    
+    switch(command_list[0]){
+        
+    case "//":
+        //  commentary case
+        return ""
+        
+    case "push":
+        //  push case
+        return push(command_list: command_list)
+        
+    case "pop":
+        //  pop case
+        return ""
+        
+    default:
+        //  default case
+        return ""
+        
+    }
+}
+
+
+
+
+
 func translate_push(command: String) -> String{
     var str: String = ""
     
